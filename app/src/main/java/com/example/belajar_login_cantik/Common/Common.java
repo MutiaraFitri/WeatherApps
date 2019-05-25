ee2a0c00ea98ba1114e62d43529481c297ebf008
codepoin <codepoin.id@gmail.com>
2019-05-26 04:12:37 +0700

    register api and make model

---
+++ app/src/main/java/com/example/belajar_login_cantik/Common/Common.java
@@ -0,0 +1,41 @@
+package com.codepoin.weatherassists.Common;
+
+import android.support.annotation.NonNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Common {
+
+    public static String API_KEY = "f4d314ae998540f9cc52e2e5de29f31d";
+    public static String API_LINK = "http://api.openweathermap.org/data/2.5/weather";
+
+    @NonNull
+    public static String apiRequest(String lang,String lat, String lng){
+        StringBuilder sb = new StringBuilder(API_LINK);
+        sb.append(String.format("?lang=%s&lat=%s&lon=%s&APPID=%s&units=metric",lang,lat,lng,API_KEY));
+        return sb.toString();
+    }
+
+    public static String unixTimeStampToDateTime(double unixTimeStamp){
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        date.setTime((long)unixTimeStamp*1000);
+        return dateFormat.format(date);
+    }
+
+    public static String getImage(String icon){
+        return String.format("https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/%s.png",icon);
+    }
+
+    public static String getDateNow(){
+        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/belajar_login_cantik/Model/Rain.java
@@ -0,0 +1,4 @@
+package com.example.belajar_login_cantik.Model;
+
+public class Rain {
+}
